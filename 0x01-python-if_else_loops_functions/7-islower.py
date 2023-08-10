4542f4239af3c8c0da7a74c0da64f1976a35ed98
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-08-10 14:37:49 -0700

    Task 7

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def islower(c):
+    """ A function that Checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
+
