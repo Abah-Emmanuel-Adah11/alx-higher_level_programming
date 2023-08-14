4292fbac75a5819daf2760599254e65ace4f4540
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-08-14 04:52:16 -0700

    Task 6 updated

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-"""A fuction that prints a matrix of integer"""
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         print(' '.join('{:d}'.format(j)for j in i))
