87a87af5c25dd8a314f595cbcdcb9cd07e39194b
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-08-14 05:17:59 -0700

    Task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    blist = my_list[:]
+    boolist = my_list[:]
     for count, i in enumerate(my_list):
         if i % 2 == 0:
-            blist[count} = True
+            boolist[count] = True
         else:
-            blist[count] = False
-        return(blist)
+            boolist[count] = False
+    return(boolist)
