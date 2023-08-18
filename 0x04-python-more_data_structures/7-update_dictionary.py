2bafd900f9024183e06ed653e94bc851815c8952
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-08-18 13:47:09 -0700

    Task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """A function that replaces or adds key/value in a dictionary."""
-
 def update_dictionary(a_dictionary, key, value):
     """Replace or add key/value pairs in a dictionary."""
     a_dictionary[key] = value
