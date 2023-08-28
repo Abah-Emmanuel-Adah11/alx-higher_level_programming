b2bea3783358eade0fac48c64cfde24cab547466
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-08-28 08:45:54 -0700

    Task 101

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -17,5 +17,5 @@ def safe_function(fct, *args):
         result = fct(*args)
         return (result)
     except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys,stderr)
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
