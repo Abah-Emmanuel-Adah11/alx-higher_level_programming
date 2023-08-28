096fba310f157babb2520cf4898a75cb508e8675
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-08-28 08:11:54 -0700

    Task 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_print_integer_err(value):
+    """A function that prints an integer with "{:d}".format().
+
+    When a VeiwError message is encountered, a corresponding message
+    is printed to standard error.
+
+    Args:
+        value (int): The integer to be printed.
+
+    Returns:
+        If a TypeError is encountered - False.
+        Else: - True.
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print(Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
