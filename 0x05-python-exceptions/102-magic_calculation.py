a2c664233e2d1c86d47b288260443816142d476b
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-08-28 09:13:22 -0700

    Task 102

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    outcome = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                outcome += a ** b / i
+            except:
+                result = b + a
+                break
+            return (outcome)
