725213bb5fb1d0636f7643d45d37bc9aa94059b4
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-08-29 14:28:32 -0700

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""A function that define Square."""
+"""Define a class Square."""
+
 
 class Square:
-def __init___(self, size):
-    """Intializing the new square.
+    """Represent a square."""
 
-    Args:
-        size (int): The size of the new square.
-    """
-    self.__size = size
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
