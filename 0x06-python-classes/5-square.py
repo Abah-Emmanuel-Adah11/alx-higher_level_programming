05159d447b7b2b3727219078a1812e7bc8b0a91a
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-08-30 04:26:51 -0700

    Task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -7,23 +7,22 @@ class Square:
     """Representing a sqaure."""
 
     def __init__(self, size):
-         """Initializing a new square.
+        """Initializing a new square.
 
-
-         Args:
+        Args:
              size (int): The size of the new square.
-         """
-         self.size = size
+        """
+        self.size = size
 
     @property
     def size(self):
-        """Get/set the current size of the sqaure."""
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
-             raise TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
@@ -39,5 +38,3 @@ class Square:
             print("")
         if self.__size == 0:
             print("")
-
-
