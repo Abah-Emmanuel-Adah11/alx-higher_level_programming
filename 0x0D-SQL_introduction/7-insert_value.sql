21ff63460ad8e63ef747043f815d12516e9c3791
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-10-17 14:24:36 -0700

    Task 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- Insert the values
+-- Insert  values
 -- Execute: cat 7-insert_ivalue.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
