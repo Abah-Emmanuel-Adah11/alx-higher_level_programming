a41a13e45cf9a3d0d1f4a2292a38e3c67cce9d5f
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-11-14 13:23:26 -0800

    Task 6 to 10

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+/* Defining the class Square */
+
+const Rectangle = require('./5-square');
+class Square extends Rectangle {
+  /* constructor call super constructor */
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      /* instance method that print the rectangle */
+      let a = this.height;
+      while (a > 0) {
+        console.log(c.repeat(this.width));
+        a--;
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+module.exports = Square;
