1f0b84dba685c501fa3f08097a05327311fafdc3
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2024-01-03 13:06:21 -0800

    Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (e, data) {
-  if (e) return consle.error(e);
+  if (e) return console.error(e);
 });
